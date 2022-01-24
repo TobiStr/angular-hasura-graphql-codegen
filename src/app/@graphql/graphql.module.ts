cd596debe3ac3805248f1562176c45aa35567ff1
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-01-24 21:20:38 +0100

    Use correct package reference

--- src/app/@graphql/graphql.module.ts
+++ src/app/@graphql/graphql.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Apollo } from 'apollo-angular';
 import { WebSocketLink } from '@apollo/client/link/ws';
-import { InMemoryCache } from '@apollo/client';
+import { InMemoryCache } from '@apollo/client/core';
 
 const GQL_WS_ENDPOINT = environment.graphqlWSEndpoint;
 
@@ -15,33 +15,35 @@ const GQL_WS_ENDPOINT = environment.graphqlWSEndpoint;
 })
 export class GraphQLModule {
   constructor(public apollo: Apollo) {
-    this.initApolloWithHeaders({ 'x-hasura-admin-secret': '<your-admin-secret>' })
-    
+    this.initApolloWithHeaders({
+      'x-hasura-admin-secret': '<your-admin-secret>',
+    });
+
     // Use this for Token based authentication
     //this.initApolloWithHeaders({ authorization: 'Bearer ' + token })
   }
 
   private initApolloWithHeaders(headers: any) {
     const wsLink = new WebSocketLink({
-			uri: GQL_WS_ENDPOINT,
-			lazy: true, // For some reason, this is required when specifying multiple headers.
-			options: {
-				reconnect: true,
-				lazy: true,
-				connectionParams: {
-					headers: headers,
-				},
-			},
-		});
+      uri: GQL_WS_ENDPOINT,
+      lazy: true, // For some reason, this is required when specifying multiple headers.
+      options: {
+        reconnect: true,
+        lazy: true,
+        connectionParams: {
+          headers: headers,
+        },
+      },
+    });
 
     this.apollo.create(
-			{
-				link: wsLink,
-				cache: new InMemoryCache(),
-				name: 'GrahpQL Showcase Client',
-				version: '0.0.0',
-			},
-			'default',
-		);
+      {
+        link: wsLink,
+        cache: new InMemoryCache(),
+        name: 'GrahpQL Showcase Client',
+        version: '0.0.0',
+      },
+      'default'
+    );
   }
 }
