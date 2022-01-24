61b01f9be2671496638cab9d7cca33fc979368b8
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-01-24 21:41:34 +0100

    Implement UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Guid } from 'guid-typescript';
 import { GraphQlService } from './@graphql/graphql.service';
 
 @Component({
@@ -7,7 +8,30 @@ import { GraphQlService } from './@graphql/graphql.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private graphQlService: GraphQlService) {}
+  public messages: string[];
 
-  
+  public message: string = 'Hello!';
+
+  constructor(private graphQlService: GraphQlService) {
+    this.graphQlService
+      .SubscribeReceivedMessages(
+        Guid.parse('e3bc9005-0e39-4b2f-a239-7a63804e7c91')
+      )
+      .subscribe(
+        (result) =>
+          (this.messages = result.user_by_pk.received_messages.map(
+            (m) => m.text
+          ))
+      );
+  }
+
+  onClicked() {
+    this.graphQlService.SendMessage(
+      Guid.parse('83c3bc24-7da6-4ac1-b3f6-cd6455e33d7d'),
+      Guid.parse('e3bc9005-0e39-4b2f-a239-7a63804e7c91'),
+      this.message
+    );
+
+    this.message = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GraphQLModule } from './@graphql/graphql.module';
 
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     GraphQLModule,
     BrowserAnimationsModule,
+    MatListModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
